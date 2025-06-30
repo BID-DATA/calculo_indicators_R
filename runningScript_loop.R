1e287bb0be01499ecc5c0a0a08f8cad243be3557
Jillie Chang <jillie.chang@gmail.com>
2025-06-30 17:58:56 -0400

    ajuste de idef.csv

--- runningScript_loop.R
+++ runningScript_loop.R
@@ -16,7 +16,7 @@ geoLevel <- "country"
 if (tipo=="encuestas"){
 
 available_years <- read.csv("Inputs/running_survey.csv") %>% 
-  filter(availability==1&person=="Jillie6") 
+  filter(availability==1&person=="Jillie10") 
 }
 
 if (tipo=="censos"){
