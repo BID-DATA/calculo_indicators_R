f54e46c8cb5ef1eab62d5f8bb3ee1f7baee63e2a
DavidCornejoArias <davidcornejoarias@gmail.com>
2026-01-15 09:33:07 -0500

    Update directory_periods.R

--- directory_periods.R
+++ directory_periods.R
@@ -3,49 +3,45 @@
 
 
 functionRoundAndSurvey <- function(pais, tipo, anio) {
-
-
-if (tipo == "encuestas") {
-
-# 1. reading dataset with surveys, round and year
-planificacionSurveys <- readxl::read_xlsx("Inputs/Planeación - Armonización de Encuestas de Hogares.xlsx", sheet = "HH surveys")
-
-# 2. Pivoting to transform excel
-# 2.1 Getting list of years availables
-yearsAvailable <- planificacionSurveys %>% 
-                  dplyr::select(where(is.numeric)) %>% 
-                  select(-"Total") %>% 
-                  colnames()
-# choosing survey depending on year and country and round depending on that information
-planificacionSurveysPivot <- planificacionSurveys %>%
-                    pivot_longer(cols=yearsAvailable,
-                                 names_to='year',
-                                 values_to='availability')
-
-# choosing round depending on that information
-round <- planificacionSurveysPivot %>% 
-         filter(`País`== pais & `year`== anio & availability ==1) %>% 
-         pull(`Ronda armonizada BID`)
-
-survey <- planificacionSurveysPivot %>% 
-          filter(`País`== pais & `year`== anio & availability ==1) %>% 
-          pull(Encuesta)
-if (pais=="VEN" & anio==2021){
   
-  base <- paste("//sapidbshares.file.core.windows.net//idbrestrictedshares//SCL_DATAFILES_RESTRICTED//harmonized//",pais,"//",survey,"//data_arm//",pais,"_",anio,round,"_BID.dta",sep = "")
-}
-else {
-  base <- paste("Z://harmonized//",pais,"//",survey,"//data_arm//",pais,"_",anio,round,"_BID.dta",sep = "")  
-}
-# return database address
-return(base)
-
-
-}
-
-if (tipo == "censos"){
-
-base <- paste("Z://census//clean//",pais,"//",pais,"_",anio,"_censusBID.dta",sep = "")  
-
-}
+  
+  if (tipo == "encuestas") {
+    
+    # 1. reading dataset with surveys, round and year
+#    planificacionSurveys <- readxl::read_xlsx("Inputs/Planeación - Armonización de Encuestas de Hogares.xlsx", sheet = "HH surveys")
+    planificacionSurveys <- read.csv("Inputs/running_survey.csv") 
+    # 2. Pivoting to transform excel
+    # 2.1 Getting list of years availables
+    
+    # choosing round depending on that information
+    round <- planificacionSurveys %>% 
+      filter(`Pais`== pais & `year`== anio & availability ==1) %>% 
+      pull(`Ronda.armonizada.BID`)
+    
+    survey <- planificacionSurveys %>% 
+      filter(`Pais`== pais & `year`== anio & availability ==1) %>% 
+      pull(Encuesta)
+    
+    restriction <- planificacionSurveys %>% 
+      filter(`Pais`== pais & `year`== anio & availability ==1) %>% 
+      pull(access_right)
+    print(restriction)
+    if (isTRUE(restriction == "restricted")) {
+      
+      base <- paste("//sapidbshares.file.core.windows.net//idbrestrictedshares//SCL_DATAFILES_RESTRICTED//harmonized//",pais,"//",survey,"//data_arm//",pais,"_",anio,round,"_BID.dta",sep = "")
+    }
+    else {
+      base <- paste("Z://harmonized//",pais,"//",survey,"//data_arm//",pais,"_",anio,round,"_BID.dta",sep = "")  
+    }
+    # return database address
+    return(base)
+    
+    
+  }
+  
+  if (tipo == "censos"){
+    
+    base <- paste("Z://census//clean//",pais,"//",pais,"_",anio,"_censusBID.dta",sep = "")  
+    
+  }
 }
