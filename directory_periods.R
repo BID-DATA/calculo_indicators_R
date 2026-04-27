13d4427e25e75fb80b4a302136f025062b0857e3
DavidCornejoArias <62267590+DavidCornejoArias@users.noreply.github.com>
2026-04-27 11:19:40 -0400

    Update directory_periods.R

--- directory_periods.R
+++ directory_periods.R
@@ -8,8 +8,8 @@ functionRoundAndSurvey <- function(pais, tipo, anio) {
   if (tipo == "encuestas") {
     
     # 1. reading dataset with surveys, round and year
-#    planificacionSurveys <- readxl::read_xlsx("Inputs/Planeación - Armonización de Encuestas de Hogares.xlsx", sheet = "HH surveys")
-    planificacionSurveys <- read.csv("Inputs/running_survey.csv") 
+    planificacionSurveys <- read.csv("Inputs/running_survey.csv")
+    
     # 2. Pivoting to transform excel
     # 2.1 Getting list of years availables
     
@@ -25,22 +25,14 @@ functionRoundAndSurvey <- function(pais, tipo, anio) {
     restriction <- planificacionSurveys %>% 
       filter(`Pais`== pais & `year`== anio & availability ==1) %>% 
       pull(access_right)
-    #print(restriction)
+    
     if (isTRUE(restriction == "restricted")) {
       
       base <- paste("//sapidbshares.file.core.windows.net//idbrestrictedshares//SCL_DATAFILES_RESTRICTED//harmonized//",pais,"//",survey,"//data_arm//",pais,"_",anio,round,"_BID.dta",sep = "")
     }
     else {
-      #base <- paste("//sapidbshares.file.core.windows.net/idbshares/SURVEYS/",pais,"//",survey,"//data_arm//",pais,"_",anio,round,"_BID.dta",sep = "")
-      #
-      base_dir <- "//sapidbshares.file.core.windows.net/idbshares/SURVEYS/harmonized"
-      base <- file.path(base_dir, pais, survey, "data_arm",
-                        sprintf("%s_%s%s_BID.dta", pais, anio, round),
-                        fsep = "/")
-      
-      #print(base)
+      base <- paste("Z://harmonized//",pais,"//",survey,"//data_arm//",pais,"_",anio,round,"_BID.dta",sep = "")  
     }
-    #\\sapidbshares.file.core.windows.net\idbshares\SURVEYS
     # return database address
     return(base)
     
